7d5597795504082bc5244a3d8870012cc47dd9fc
elthon urbina <uelthon95@gmail.com>
2023-05-07 12:58:51 -0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  experimental: {
+    runtime: 'experimental-edge'
+  }
 }
 
 module.exports = nextConfig
---
+++ src/pages/character/[id].js
@@ -0,0 +1,21 @@
+import Hero from '@/components/Hero'
+import api from '@/services/api'
+import React from 'react'
+
+export default function Character ({ data }) {
+  return (
+    <div>
+      {data?.id && <Hero image={data.image} name={data.name} />}
+    </div>
+  )
+}
+
+export async function getServerSideProps (context) {
+  const { id = 1 } = context.params
+  const data = await api.getOne(id)
+  return {
+    props: {
+      data
+    }
+  }
+}
